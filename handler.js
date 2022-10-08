31ec68f767a4823a50ec8c96ee765bec11abb89d
Quyen Tat <tvq9612@gmail.com>
2022-10-08 17:15:08 +0700

    add redis

--- handler.js
+++ handler.js
@@ -1,7 +1,15 @@
 "use strict";
 const fetch = require("node-fetch");
 
+// redis
+const { createClient } = require("redis");
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+
 module.exports.stockInfo = async (event) => {
+  await client.connect();
+  const PREFIX_REDIS_KEY = "vietstock_";
   const query = event.queryStringParameters;
   if (query === undefined) {
     return {
@@ -16,22 +24,30 @@ module.exports.stockInfo = async (event) => {
       ),
     };
   }
-  const response = await fetch(
-    "https://finance.vietstock.vn/company/tradinginfo",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        Cookie: "__RequestVerificationToken=" + process.env.COOKIE,
-      },
-      body:
-        "code=" +
-        query.ticket +
-        "&s=0&t=&__RequestVerificationToken=" +
-        process.env.BODY_TOKEN,
-    }
-  );
-  let body = await response.json();
+  let body = await client.get(PREFIX_REDIS_KEY + query.ticket);
+  body = JSON.parse(body);
+  if (body === null) {
+    const response = await fetch(
+      "https://finance.vietstock.vn/company/tradinginfo",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Cookie: "__RequestVerificationToken=" + process.env.COOKIE,
+        },
+        body:
+          "code=" +
+          query.ticket +
+          "&s=0&t=&__RequestVerificationToken=" +
+          process.env.BODY_TOKEN,
+      }
+    );
+    body = await response.json();
+    await client.set(PREFIX_REDIS_KEY + query.ticket, JSON.stringify(body), {
+      EX: 60 * 5,
+    });
+  }
+  await client.quit();
 
   return {
     statusCode: 200,
